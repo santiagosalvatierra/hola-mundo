0a3b6f3074ff3c126783471fb6e33e9e7f29896f
CyntiaDalesio <cyntiam.135@gmail.com>
2021-09-30 22:59:16 -0300

    Cambios

--- HolaMundo/src/holamundo/Perro.java
+++ HolaMundo/src/holamundo/Perro.java
@@ -7,15 +7,27 @@ public class Perro {
     private String nombre;
     private String raza;
     private Integer edad;
+    private String tamaño;
 
-    public Perro() {
-    }
-
-    public Perro(String nombre, String raza, Integer edad) {
+    public Perro(String nombre, String raza, Integer edad, String tamaño) {
         this.nombre = nombre;
         this.raza = raza;
         this.edad = edad;
+        this.tamaño = tamaño;
     }
+    
+    public Perro() {
+    }
+
+    public String getTamaño() {
+        return tamaño;
+    }
+
+    public void setTamaño(String tamaño) {
+        this.tamaño = tamaño;
+    }
+
+
 
     public String getNombre() {
         return nombre;
@@ -43,10 +55,9 @@ public class Perro {
 
     @Override
     public String toString() {
-        return "Perro{" + "nombre=" + nombre + ", raza=" + raza + ", edad=" + edad + '}';
+        return "Perro{" + "nombre=" + nombre + ", raza=" + raza + ", edad=" + edad + ", tama\u00f1o=" + tamaño + '}';
     }
-    
-    
+
     
     
     
