c8facd53c269afbefbc1d751bb1851758232c0a1
Sebastian <seba_al2007@hotmail.com>
2021-10-04 17:32:01 -0300

    cargar ahora

---
+++ HolaMundo/src/holamundo/Toro.java
@@ -0,0 +1,46 @@
+package holamundo;
+public class Toro {
+  private String nombre;
+  private String raza;
+  private Integer peso;
+
+    public Toro() {
+    }
+
+    public Toro(String nombre, String raza, Integer peso) {
+        this.nombre = nombre;
+        this.raza = raza;
+        this.peso = peso;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getRaza() {
+        return raza;
+    }
+
+    public void setRaza(String raza) {
+        this.raza = raza;
+    }
+
+    public Integer getPeso() {
+        return peso;
+    }
+
+    public void setPeso(Integer peso) {
+        this.peso = peso;
+    }
+  
+  
+    
+    
+    
+    
+    
+}
